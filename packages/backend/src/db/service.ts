4d9819691031ab03704ee063a8bd8b9bed83570a
Shaurya Gupta <118044473+Shaurya10101@users.noreply.github.com>
2025-03-02 02:01:20 +0530

    dockerized the database (#28)
    * dockerized the database
    
    Signed-off-by: Shaurya10101 <shauryagupta8046@gmail.com>
    
    * Inject auth context into tanstack router (#23)
    
    Signed-off-by: Kurt King <kurtaking@gmail.com>
    Signed-off-by: Shaurya10101 <shauryagupta8046@gmail.com>
    
    ---------
    
    Signed-off-by: Shaurya10101 <shauryagupta8046@gmail.com>
    Signed-off-by: Kurt King <kurtaking@gmail.com>
    Co-authored-by: Kurt King <kurtaking@gmail.com>

--- packages/backend/src/db/service.ts
+++ packages/backend/src/db/service.ts
@@ -4,14 +4,14 @@ import postgres from 'postgres';
 import * as authSchema from './schema/auth-schema';
 import * as eventsSchema from './schema/events-schema';
 import * as personasSchema from './schema/personas-schema';
-
-config({ path: '.env' });
-
-const url = process.env.DATABASE_URL;
-
-if (!url) {
+   
+config({ path: '.env' });  
+ 
+const url = process.env.DATABASE_URL; 
+if (!url) { 
   throw new Error('DATABASE_URL is not set');
 }
+ 
 
 const client = postgres(url);
 export const nurseryDb = drizzle({ client, schema: { ...authSchema, ...personasSchema, ...eventsSchema } });
